8c69f5c318ab541084e4603b90680b5ed873445f
ZhenyaKasatkina <knopisha.zh@gmail.com>
2023-11-30 00:24:42 +0300

    add def subtract

--- funcs.py
+++ funcs.py
@@ -4,5 +4,11 @@ def sum_up(num_1, num_2):
     return sum_numbers
 
 
+def subtract(num_1, num_2):
+    difference_numbers = num_1 - num_2
+    return difference_numbers
+
+
 number_x = sum_up(1, 2)
-print(number_x)
+number_y = sum_up(5, 2)
+print(number_x, number_y)
